f7c534df4e5ae080d741c3a01b80805f2c8c0798
RAVISHANKAR J <gceravishankar@gmail.com>
2025-12-20 13:53:38 +0530

    .

--- _plugins/watcher-patch.rb
+++ _plugins/watcher-patch.rb
@@ -1,4 +1,4 @@
-# frozer_string_literal: true
+# frozen_string_literal: true
 
 require 'jekyll/watch'
 
@@ -7,7 +7,6 @@ module Jekyll
         extend self
 
         alias_method :original_listen_ignore_paths, :listen_ignore_paths
-        
         def listen_ignore_paths(options)
             original_listen_ignore_paths(options) + [%r!.*\.TMP!i]
         end
